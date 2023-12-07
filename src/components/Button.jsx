78149f6c884bb0a829abbbb654cc91c70ad29718
leandre <tuyambazeleandre@gmail.com>
2023-12-07 10:47:48 +0200

    (super-quality): implement super quality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,11 +8,13 @@ const Button = ({ label, iconURL }) => {
       font-monteserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red"
     >
       {label}
-      <img
-        src={iconURL}
-        alt="allow right icon"
-        className="ml-2 rounded-full w-5 h-5"
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="allow right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
   );
 };
