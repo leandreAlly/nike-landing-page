2715f9c9e1508123be7c4a436054d96d28b6a837
leandre <tuyambazeleandre@gmail.com>
2023-12-07 11:17:51 +0200

    (services-card): implement services card

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -53,9 +53,10 @@ const Hero = () => {
           className="object-contain relative z-10"
         />
         <div className="flex sm:gap-6 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
-          {shoes.map((shoe) => (
-            <div>
+          {shoes.map((shoe, index) => (
+            <div key={index}>
               <ShoeCard
+                index={index}
                 imgURL={shoe}
                 changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
                 bigShoeImg={bigShoeImg}
