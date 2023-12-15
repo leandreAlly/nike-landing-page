20434566c97fb2cf75fc9331198c6763ad15c22c
leandre <tuyambazeleandre@gmail.com>
2023-12-15 19:32:38 +0200

    (customer-reviews): add title for section

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,9 +1,17 @@
-
-
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
-  )
-}
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our
+        <span className="text-coral-red"> Customers </span>
+        Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Hear genuine stories from our satisfied customers about their
+        exceptional experiences with us.
+      </p>
+    </section>
+  );
+};
 
-export default CustomerReviews
+export default CustomerReviews;
